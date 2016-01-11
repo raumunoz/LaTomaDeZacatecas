81d98ee3513a2cbe14d66028d6aa87a4eac2df6e
raudel <c.sa.r@hotmail.com>
2016-01-11 11:45:03 -0600

    cambios rau

---
+++ pruebas/Assets/Scripts/moviescreditos.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var movTexture : MovieTexture;
+var eliminador:GameObject;
+
+function Start () {
+	destructor();
+	GetComponent.<Renderer>().material.mainTexture = movTexture;
+
+	var cam:Camera  = Camera.main; 
+    var pos:float = (cam.nearClipPlane + 0.01f);
+    transform.position = cam.transform.position + cam.transform.forward * pos;
+    transform.LookAt (cam.transform);
+    transform.Rotate (90.0f, 0.0f, 0.0f);  
+    var  h:float = (Mathf.Tan(cam.fieldOfView*Mathf.Deg2Rad*0.5f)*pos*2f) /10.0f; 
+    transform.localScale = new Vector3(h*cam.aspect,1.0f, h);
+    
+    var aud: AudioSource = GetComponent.<AudioSource>();  
+	aud.clip = movTexture.audioClip;
+	
+	movTexture.Play();
+	
+	aud.Play();
+
+}
+
+function Update () {
+if(movTexture.isPlaying != true){
+         	Application.LoadLevel(0);
+         }
+
+}
+
+function destructor(){
+ eliminador = GameObject.Find("Música");
+ Destroy(eliminador);
+}
+
---
+++ pruebas/Assets/Scripts/musica.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+var uno:AudioClip;
+var dos:AudioClip;
+var tres:AudioClip;
+var cuatro:AudioClip;
+var cinco:AudioClip;
+var seis:AudioClip;
+var numero:int;
+
+
+
+function Start () {
+	musica();
+}
+
+function Update () {
+
+}
+
+function Awake(){
+DontDestroyOnLoad(transform.gameObject);
+if(FindObjectsOfType(GetType()).Length>1){
+Destroy(gameObject);
+}
+}
+
+
+function musica (){
+var audiocancion:AudioSource = GetComponent.<AudioSource>();
+	numero = 0;
+	numero += Random.Range(0,5);
+	Debug.Log(numero);
+	switch(numero){
+	case 0: audiocancion.clip= uno;
+        	audiocancion.Play();
+        	break;
+    case 1: audiocancion.clip= dos;
+        	audiocancion.Play();
+        	break;
+    case 2: audiocancion.clip= tres;
+        	audiocancion.Play();
+        	break;
+    case 3: audiocancion.clip= cuatro;
+        	audiocancion.Play();
+        	break;
+    case 4: audiocancion.clip= cinco;
+        	audiocancion.Play();
+        	break;
+    default: audiocancion.clip= seis;
+        	 audiocancion.Play();
+        break;
+
+}
+}
